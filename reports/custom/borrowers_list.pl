359ac5a9e28c786915b9e7d18c22543c5a9e804b
Michael Hafen <mdhafen@tech.washk12.org>
2008-09-04 16:12:49 -0600

    Beginning of the WCSD custom reports
    This is the beginning of my custom reports for WCSD.
    Eventually I will remove (comment out) all the stock reports, because I
    don't like any of them.

---
+++ reports/custom/borrowers_list.pl
@@ -0,0 +1,375 @@
+#!/usr/bin/perl
+
+# Copyright 2000-2002 Katipo Communications
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+
+# Report template created by Michael Hafen (mdhafen@tech.washk12.org) for WCSD
+# This is designed for Koha3
+# This is designed to use a single template file for any report created with it.
+
+use strict;
+use CGI;
+use C4::Auth;
+use C4::Context;
+use C4::Output;
+use C4::Branch;  # GetBranches GetBranchInfo
+use C4::Members;  # GetMemberSortValues
+use C4::Koha;
+use C4::Circulation;
+use C4::Dates qw/format_date_in_iso/;
+
+my $accesses_borrowers = 0;  # bool to indicate this report uses the borrowers table
+
+if ( $accesses_borrowers && C4::Context->preference('MembersViaExternal') ) {
+    use C4::MembersExternal;
+}
+
+# Watch out for:
+#  C4::Context->preference('MembersViaExternal')
+#  C4::Context->preference("IndependantBranches")
+
+my $input = new CGI;
+my $dbh = C4::Context->dbh;
+
+my $reportname = "borrowers_list";  # ie "collection_itemnums"
+my $reporttitle = "Borrowers List";  # ie "Item Number by Branch"
+my @columns = ( "branchcode", "borrowers.cardnumber", "borrowers.surname", "borrowers.firstname", "borrowers.cardnumber" );
+my @column_titles = ( "School", "Cardnumber", "Last Name", "First Name" );  # ie "Title"
+my @tables = ( "borrowers",  # ie "items"
+#	       [ # Cross Joined Tables
+#	         {
+#	           table => '',  # Table name
+#	           using => '',  # Using column
+#	           on_l => '',   # Left hand Join On column
+#	           on_r => '',   # Right hand Join On column
+#	         },
+#	       ],
+#	       [ # Left Joined Tables
+#	         {
+#	           table => '',  # Table name
+#	           using => '',  # Using column
+#	           on_l => '',   # Left hand Join On column
+#	           on_r => '',   # Right hand Join On column
+#	         },
+#	       ],
+	       );
+
+#FIXME build queryfilter
+my @filters = $input->param("Filter");
+my @queryfilter = ();
+if ( $filters[0] ) {
+    push @queryfilter, { crit => "borrowers.sort1", op => "=", filter => $dbh->quote( $filters[0] ), title => "Graduation Date", value => $filters[0] };
+    unshift @columns, "borrowers.sort1";
+    unshift @column_titles, "Graduation Date";
+}
+if ( $filters[1] ) {
+    push @queryfilter, { crit => "borrowers.sort2", op => "=", filter => $dbh->quote( $filters[1] ), title => "Homeroom Teacher", value => $filters[1] };
+    unshift @columns, "borrowers.sort2";
+    unshift @column_titles, "Homeroom Teacher";
+}
+
+my @loopfilter = ();
+
+my $where = "1 = 1";
+my $order = "$columns[0]";
+
+if ( $order = $input->param("Order") ) {
+    for ( $order ) {
+	if    ( /bhn/ ) { $order = "branchcode,sort2,surname,firstname"; }
+	elsif ( /bn/ )  { $order = "branchcode,surname,firstname"; }
+	else            { $order = "branchcode,cardnumber"; }
+    }
+}
+
+# Rest of params
+my $do_it=$input->param('do_it');
+my $output = $input->param("output");
+# rfc822 references 65 characters as limit for body of header fields
+# substr here is to prevent header overflow attacks against browsers
+my $basename = substr( $input->param("basename"), 0, 65 );
+my $sep = $input->param( "sep" );
+#my $mime = $input->param("MIME");
+my $mime = 'application/vnd.sun.xml.calc';  # There is really only one option
+
+my ($template, $borrowernumber, $cookie)
+	= get_template_and_user({template_name => "reports/custom_report.tmpl",
+				query => $input,
+				type => "intranet",
+				authnotrequired => 0,
+				flagsrequired => {management => 1, tools => 1},
+				debug => 0,
+				});
+
+my $userenv = C4::Context->userenv;
+
+$template->param(
+		 do_it => $do_it,
+		 reportname => $reportname,
+		 reporttitle => $reporttitle,
+		 DHTMLcalendar_dateformat => C4::Dates->DHTMLcalendar(),
+		 );
+
+if ($do_it) {
+	my $results = calculate( \@columns, \@column_titles, \@tables, $where, $order, \@queryfilter, \@loopfilter );
+	if ($output eq "screen"){
+		$template->param(mainloop => $results);
+		output_html_with_http_headers $input, $cookie, $template->output;
+	} else {
+		print $input->header(-type => $mime,
+					 -attachment=>"$basename.csv",
+					 -name=>"$basename.csv" );
+		my @headers = @{ $$results[0]->{loopheader} };
+		my @lines = @{ $$results[0]->{looprow} };
+		for ( $sep ) {
+			if    ( m/tab/ ) { $sep = "\t"; }
+			elsif ( m/\\/  ) { $sep = "\\"; }
+			elsif ( m/\//  ) { $sep = "\/"; }
+			elsif ( m/,/   ) { $sep = ",";  }
+			elsif ( m/\#/  ) { $sep = "#";  }
+			else             { $sep = ";";  }
+		}
+
+		foreach my $line ( @headers ) {
+			foreach my $cell ( @{ $line->{values} } ) {
+				print $$cell{value}.$sep;
+				print $sep x ( $$cell{width} - 1 ) if ( $$cell{width} );
+			}
+			print "\n";
+		}
+		foreach my $line ( @lines ) {
+			foreach my $cell ( @{ $line->{values} } ) {
+				print $$cell{value}.$sep;
+				print $sep x ( $$cell{width} - 1 ) if ( $$cell{width} );
+			}
+			print "\n";
+		}
+	}
+} else {
+	# FIXME  Fill in other dropdowns
+
+	my @parameters;
+
+	my ( $sort1_values, $sort2_values ) = GetMemberSortValues();
+	my ( @sort1_loop, @sort2_loop );
+	foreach ( sort @$sort1_values ) {
+	    push @sort1_loop, { value => $_, label => $_ } if ( $_ );
+	}
+	foreach ( sort @$sort2_values ) {
+	    push @sort2_loop, { value => $_, label => $_ } if ( $_ );
+	}
+	push @parameters, {
+	    select_box => 1,
+	    select_loop => \@sort1_loop,
+	    label => "sort1",
+	    first_blank => 1,
+	};
+	push @parameters, {
+	    select_box => 1,
+	    select_loop => \@sort2_loop,
+	    label => "sort2",
+	    first_blank => 1,
+	};
+
+	my @order_loop;
+	push @order_loop, { value => 'bhn', label => 'Homeroom Teacher' };
+	push @order_loop, { value => 'bn', label => 'Name' };
+	push @order_loop, { value => 'bc', label => 'Cardnumber' };
+	push @parameters, {
+	    select_box => 1,
+	    input_name => 'Order',
+	    select_loop => \@order_loop,
+	    label => 'Sort By',
+	};
+
+	my @dels = ( ";", "tabulation", "\\", "\/", ",", "\#" );
+	foreach my $limiter ( @dels ) {
+	    my $selected = ( $limiter eq C4::Context->preference("delimiter") );
+	    $limiter = { value => $limiter, selected => $selected };
+	}
+
+	$template->param(
+	    parameter_loop => \@parameters,
+	    sep_loop => \@dels,
+	    );
+
+	output_html_with_http_headers $input, $cookie, $template->output;
+}
+
+sub calculate {
+	my ($columns, $column_titles, $tables, $where, $order, $qfilters, $lfilters) = @_;
+
+	my $dbh = C4::Context->dbh;
+	my @looprow;
+	my @loopheader;
+	my %globalline;
+	my @mainloop;
+	my $grantotal = 0;
+	my @big_loop;
+
+	my $table = shift @$tables;
+	my $column = join ',', @$columns;
+
+	$where = "1" unless ( $where );
+
+	my $query = "SELECT DISTINCT $column FROM $table ";
+	if ( @$tables ) {
+	    if ( my @cross = @{ shift @$tables } ) {
+		foreach my $cross_table ( @cross ) {
+		    $query .= "CROSS JOIN $$cross_table{table} ";
+		    if ( $$cross_table{using} ) {
+			$query .= "USING ($$cross_table{using}) ";
+		    } elsif ($$cross_table{on_l} and $$cross_table{on_r}) {
+			$query .= "ON $$cross_table{on_l} = $$cross_table{on_r} ";
+		    } else {
+			warn "$reportname : don't know how to join table $$cross_table{table}";
+		    }
+		}
+	    }
+	    if ( my @left = @{ shift @$tables } ) {
+		foreach my $left_table ( @left ) {
+		    $query .= "LEFT JOIN $$left_table{table} ";
+		    if ( $$left_table{using} ) {
+			$query .= "USING ($$left_table{using}) ";
+		    } elsif ($$left_table{on_l} and $$left_table{on_r}) {
+			$query .= "ON $$left_table{on_l} = $$left_table{on_r} ";
+		    } else {
+			warn "$reportname : don't know how to join table $$left_table{table}";
+		    }
+		}
+	    }
+	}
+	$query .= "WHERE $where ";
+
+	if ( @$qfilters ) {
+	    foreach ( @$qfilters ) {
+		if ( $accesses_borrowers && C4::Context->preference('MembersViaExternal') && $$_{crit} =~ m/^borrowers\.(.*?)$/i ) {
+		    my %okfields = (
+				    borrowernumber => 1,
+				    cardnumber => 1,
+				    surname => 1,
+				    firstname => 1,
+				    othernames => 1,
+				    branchcode => 1,
+				    );
+		    #  External fields will have to be handled down in the loop
+		    unless ( $okfields{ $1 } ) {
+			#  leading and trailing ' will muddle MembersExternal
+			$$_{filter} =~ s/^\'(.*)\'$/$1/;
+			push @$lfilters, { crit => $$_{crit}, filter => $$_{filter} };
+		    } else {
+			$query .= "AND $$_{crit} $$_{op} $$_{filter} ";
+		    }
+		} else {
+		    $query .= "AND $$_{crit} $$_{op} $$_{filter} ";
+		}
+	    }
+	}
+
+	$query .= "ORDER BY $order" if ( $order );
+
+	my $sth_col = $dbh->prepare( $query );
+	$sth_col->execute();
+
+CALC_MAIN_LOOP:
+	while ( my ( @values ) = $sth_col->fetchrow ) {
+	    #  This block is to handle MembersViaExternal stuff, and
+	    #   for other custom loop filters
+	    if ( @$lfilters ) {
+		my %external_bor_fields;
+		foreach ( @$lfilters ) {
+		    if ( $accesses_borrowers && C4::Context->preference('MembersViaExternal') && $$_{crit} =~ m/^borrowers\.(.*?)$/i ) {
+			$external_bor_fields{ $1 } = $$_{filter};
+		    } else {
+			# Process other loop filters here
+		    }
+
+		}
+		if ( %external_bor_fields ) {
+		    # FIXME borrowers.cardnumber needs to be last in the columns for this
+		    my $cardnumber = $values[ $#values ];
+		    my $temp = GetMemberDetails_External( $cardnumber );
+
+		    foreach ( sort keys %external_bor_fields ) {
+			next CALC_MAIN_LOOP if ( $external_bor_fields{$_} ne $temp->{$_} );
+		    }
+		}
+	    }
+	    push @big_loop, \@values;
+	}
+
+	# FIXME Sort big_loop here
+	#  This is necessary if MembersViaExternal is on and
+	#  there are borrowers fields ( ie sort1 or sort2 ) in the order clause
+
+	if ( $order =~ /sort2/ ) {
+	    my $num = 0;  # sort2 is always [0], others might be offset by sort1
+	    $num = 1 if ( $$columns[1] eq 'borrowers.sort1' );
+	    my $sort_func = sub {
+		( uc $$a[ $num+1 ] cmp uc $$b[ $num+1 ] ) ||
+		    ( uc $$a[0] cmp uc $$b[0] ) ||
+		    ( uc $$a[ $num+3 ] cmp uc $$b[ $num+3 ] ) ||
+		    ( uc $$a[ $num+4 ] cmp uc $$b[ $num+4 ] )
+	    };
+	    @big_loop = sort $sort_func @big_loop;
+	}
+
+	foreach my $data ( @big_loop ) {
+	    my %row;
+	    my @mapped_values;
+	    my @values = @$data;
+	    foreach ( @values[ 0 .. $#$column_titles ] ) {
+		push @mapped_values, { value => $_ };
+	    }
+	    $row{ 'values' } = \@mapped_values;
+	    push @looprow, \%row;
+	    $grantotal++;
+	}
+
+	foreach ( @$column_titles ) {
+	    push @{ $loopheader[0]->{ 'values' } }, { 'coltitle' => $_ };
+	}
+
+	# the filters used
+	$globalline{loopfilter} = $qfilters;
+
+	# the header of the table
+	$globalline{loopheader} = \@loopheader;
+
+	# FIXME comment out these seven lines to remove the total row
+	# the foot (totals)
+	push @looprow, {
+			 'values' => [
+				       {
+					   'width' => @$column_titles - 1,
+					   'value' => '&nbsp;',
+					   'header' => 1,
+				       },
+				       {
+					   'value' => $grantotal,
+					   'header' => 1,
+				       }
+				     ]
+		       };
+
+	# the core of the table
+	$globalline{looprow} = \@looprow;
+
+
+	push @mainloop, \%globalline;
+
+	return \@mainloop;
+}
