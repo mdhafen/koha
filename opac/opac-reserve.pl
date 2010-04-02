1cad6aacb69da68b96deabf364a1b6895c2fec0e
Michael Hafen <mdhafen@tech.washk12.org>
2010-04-02 10:29:26 -0600

    only send email for reserves on opac if there are items on shelf (not issued)
    Check the issues table to see if there are any items no on loan.  If there are
    items not on loan assume they are available and go ahead and send the email
    so the librarian knows to pull the copy.
    
    Should also check not-for-loan and lost status.  Maybe next patch.

--- opac/opac-reserve.pl
+++ opac/opac-reserve.pl
@@ -207,36 +207,50 @@ if ( $query->param('place_reserve') ) {
         my $letter_code = ( $checkitem ) ? 'PLACED_TARGETTED' : 'PLACED';
         my $letter = C4::Letters::getletter( 'reserves', $letter_code );
         if ( $letter ) {
+            my $sth;
             $sql = qq/
-            SELECT borrowernumber
-              FROM borrowers
-             WHERE email = ?
-                OR emailpro = ?
-                OR B_email = ?
-            /;
-            my $sth = $dbh->prepare($sql);
-            $sth->execute( $to_address, $to_address, $to_address );
-            my ( $real_borrowernumber ) = $sth->fetchrow;
+            SELECT COUNT(*)
+              FROM items
+         LEFT JOIN issues USING (itemnumber)
+             WHERE biblionumber = ?
+               AND issues.date_due IS NULL
+/;
+            $sth = $dbh->prepare( $sql );
+            $sth->execute( $biblioNum );
+            ( $sql ) = $sth->fetchrow();
+            if ( $sql > 0 ) {
+                $sth->finish();
+                $sql = qq/
+                SELECT borrowernumber
+                  FROM borrowers
+                 WHERE email = ?
+                    OR emailpro = ?
+                    OR B_email = ?
+                /;
+                $sth = $dbh->prepare($sql);
+                $sth->execute( $to_address, $to_address, $to_address );
+                my ( $real_borrowernumber ) = $sth->fetchrow;
 
-            C4::Letters::parseletter( $letter, 'branches', $branch );
-            C4::Letters::parseletter( $letter, 'borrowers', $borrowernumber );
-            C4::Letters::parseletter( $letter, 'biblio', $biblionumber );
-            C4::Letters::parseletter( $letter, 'reserves', $borrowernumber, $biblionumber );
-            if ( $checkitem ) {
-                C4::Letters::parseletter( $letter, 'items', $checkitem );
-            }
-
-            $letter->{'title'}   =~ s/<<[a-z0-9_]+\.[a-z0-9]+>>//g;
-            $letter->{'content'} =~ s/<<[a-z0-9_]+\.[a-z0-9]+>>//g;
-
-            C4::Letters::EnqueueLetter(
-                {   letter                 => $letter,
-                    borrowernumber         => ( $real_borrowernumber ? $real_borrowernumber : $borrowernumber ),
-                    to_address             => $branch_details->{'branchemail'},
-                    message_transport_type => 'email',
-                    from_address           => $admin_address,
+                C4::Letters::parseletter( $letter, 'branches', $branch );
+                C4::Letters::parseletter( $letter, 'borrowers', $borrowernumber );
+                C4::Letters::parseletter( $letter, 'biblio', $biblioNum );
+                C4::Letters::parseletter( $letter, 'reserves', $borrowernumber, $biblioNum );
+                if ( $checkitem ) {
+                    C4::Letters::parseletter( $letter, 'items', $checkitem );
                 }
-            );
+
+                $letter->{'title'}   =~ s/<<[a-z0-9_]+\.[a-z0-9]+>>//g;
+                $letter->{'content'} =~ s/<<[a-z0-9_]+\.[a-z0-9]+>>//g;
+
+                C4::Letters::EnqueueLetter(
+                    {   letter                 => $letter,
+                        borrowernumber         => ( $real_borrowernumber ? $real_borrowernumber : $borrowernumber ),
+                        to_address             => $branch_details->{'branchemail'},
+                        message_transport_type => 'email',
+                        from_address           => $admin_address,
+                    }
+                );
+            }
         }
     }
 
