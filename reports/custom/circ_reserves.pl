6bba123ea1fe11bb0335aca946305d8752b3e3a0
Michael Hafen <michael.hafen@washk12.org>
2016-11-30 10:20:20 -0700

    custom report: title on hold - have available items count check item flags
    Make sure an item isn't lost, restricted, or withdrawn when counting
    available items.

--- reports/custom/circ_reserves.pl
+++ reports/custom/circ_reserves.pl
@@ -55,7 +55,7 @@ my ($template, $borrowernumber, $cookie)
 
 my $reportname = "circ_reserves";
 my $reporttitle = "Titles on Hold";
-my @columns = ( "CONCAT( borrowers.surname, ', ', borrowers.firstname ) AS borrower", "CONCAT_WS(' ', biblio.title, biblio.remainderoftitle ) AS title", "GROUP_CONCAT( DISTINCT itemcallnumber SEPARATOR ',' ) AS callnumbers", "MIN(reservedate) AS reservedate", "MIN(priority) AS priority", "( SELECT COUNT(*) FROM items WHERE biblionumber = reserves.biblionumber AND items.onloan IS NULL ) AS available", "reserves.biblionumber" );
+my @columns = ( "CONCAT( borrowers.surname, ', ', borrowers.firstname ) AS borrower", "CONCAT_WS(' ', biblio.title, biblio.remainderoftitle ) AS title", "GROUP_CONCAT( DISTINCT itemcallnumber SEPARATOR ',' ) AS callnumbers", "MIN(reservedate) AS reservedate", "MIN(priority) AS priority", "( SELECT COUNT(*) FROM items WHERE biblionumber = reserves.biblionumber AND items.onloan IS NULL AND COALESCE(items.itemlost,0) = 0 AND COALESCE(items.restricted,0) = 0 AND COALESCE(items.wthdrawn,0) = 0 ) AS available", "reserves.biblionumber" );
 my @column_titles = ( "Patron", "Title", "Call Number(s)", "Date Placed", "Priority", "Copies Available" );
 my @tables = ( "reserves",
 	       [ # Cross Joined Tables
