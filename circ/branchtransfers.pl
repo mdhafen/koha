71f6e7f45f6f37f81aba060c7b589a34157ecbf1
Michael Hafen <michael.hafen@washk12.org>
2021-06-08 16:47:13 -0600

    Make item barcode unique just in homebranch
    Had to change a lot of sql and Koha::Item->find stuff to include homebranch.
    This is to enable migration of multiple sites from individual systems into
    a shared Koha instance.

--- circ/branchtransfers.pl
+++ circ/branchtransfers.pl
@@ -135,7 +135,11 @@ if ( $op eq 'cud-transfer' && $barcode ) {
             trigger         => $trigger
         }
     );
-    my $item = Koha::Items->find( { barcode => $barcode } );
+    my $item_filter = { barcode => $barcode };
+    if ( C4::Context->preference('IndependentBranches') ) {
+        $item_filter->{homebranch} = C4::Context->userenv->{'branch'};
+    }
+    my $item = Koha::Items->find($item_filter);
     $found = $messages->{'ResFound'} unless $settransit;
     if ($transferred) {
         my %trsfitem;
@@ -159,7 +163,11 @@ foreach ( $query->param ) {
     $trsfitem{counter}  = $counter;
     $trsfitem{frombrcd} = $frbcd;
     $trsfitem{tobrcd}   = $tobcd;
-    my $item = Koha::Items->find( { barcode => $bc } );
+    my $item_filter = { barcode => $bc };
+    if ( C4::Context->preference('IndependentBranches') ) {
+        $item_filter->{homebranch} = C4::Context->userenv->{'branch'};
+    }
+    my $item = Koha::Items->find($item_filter);
     $trsfitem{item} = $item;
     push( @trsfitemloop, \%trsfitem );
 }
