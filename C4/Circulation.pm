c6e74ea8679d7829f3eb9069bbfa5503bb738ac9
Michael Hafen <michael.hafen@washk12.org>
2021-06-08 16:47:13 -0600

    Make item barcode unique just in homebranch
    Had to change a lot of sql and Koha::Item->find stuff to include homebranch.
    This is to enable migration of multiple sites from individual systems into
    a shared Koha instance.

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -326,7 +326,11 @@ sub transferbook {
     my $trigger  = $params->{trigger};
     my $messages;
     my $dotransfer      = 1;
-    my $item = $barcode ? Koha::Items->find( { barcode => $barcode } ) : undef;
+    my $item_filter = { barcode => $barcode };
+    if ( C4::Context->preference('IndependentBranches') ) {
+        $item_filter->{homebranch} = C4::Context->userenv->{'branch'};
+    }
+    my $item = $barcode ? Koha::Items->find( $item_filter ) : undef;
 
     Koha::Exceptions::MissingParameter->throw(
         "Missing mandatory parameter: from_branch")
@@ -780,8 +784,12 @@ sub CanBookBeIssued {
     my $onsite_checkout     = $params->{onsite_checkout}     || 0;
     my $override_high_holds = $params->{override_high_holds} || 0;
 
+    my $item_filter = { barcode => $barcode };
+    if ( C4::Context->preference('IndependentBranches') ) {
+        $item_filter->{homebranch} = C4::Context->userenv->{'branch'};
+    }
     my $item_object = !$barcode ? undef : $params->{item}
-      // Koha::Items->find( { barcode => $barcode } );
+      // Koha::Items->find( $item_filter );
 
     # MANDATORY CHECKS - unless item exists, nothing else matters
     unless ( $item_object ) {
@@ -1574,7 +1582,11 @@ sub AddIssue {
     # Stop here if the patron or barcode doesn't exist
     if ( $patron && $barcode && $barcodecheck ) {
         # find which item we issue
-        my $item_object = Koha::Items->find({ barcode => $barcode })
+        my $item_filter = { barcode => $barcode };
+        if ( C4::Context->preference('IndependentBranches') ) {
+            $item_filter->{homebranch} = C4::Context->userenv->{'branch'};
+        }
+        my $item_object = Koha::Items->find( $item_filter )
           or return;    # if we don't get an Item, abort.
         my $item_unblessed = $item_object->unblessed;
 
@@ -2142,7 +2154,11 @@ sub AddReturn {
     my $stat_type = 'return';
 
     # get information on item
-    my $item = Koha::Items->find({ barcode => $barcode });
+    my $item_filter = { barcode => $barcode };
+    if ( C4::Context->preference('IndependentBranches') ) {
+        $item_filter->{homebranch} = C4::Context->userenv->{'branch'};
+    }
+    my $item = Koha::Items->find( $item_filter );
     unless ($item) {
         return ( 0, { BadBarcode => $barcode } );    # no barcode means no item or borrower.  bail out.
     }
@@ -4042,6 +4058,10 @@ my $query=qq|SELECT count(*)
 	     FROM items 
              WHERE barcode=?
 	    |;
+if ( C4::Context->preference('IndependentBranches') ) {
+    $query .= qq|
+               AND homebranch = '| . C4::Context->userenv->{'branch'} . qq|'|;
+}
 my $sth = $dbh->prepare($query);
 $sth->execute($barcode);
 my $exist=$sth->fetchrow ;
@@ -4252,7 +4272,11 @@ sub ProcessOfflineOperation {
 sub ProcessOfflineReturn {
     my $operation = shift;
 
-    my $item = Koha::Items->find({barcode => $operation->{barcode}});
+    my $item_filter = { barcode => $operation->{barcode} };
+    if ( C4::Context->preference('IndependentBranches') ) {
+        $item_filter->{homebranch} = C4::Context->userenv->{'branch'};
+    }
+    my $item = Koha::Items->find( $item_filter );
 
     if ( $item ) {
         my $itemnumber = $item->itemnumber;
@@ -4283,7 +4307,7 @@ sub ProcessOfflineIssue {
     $patron ||= Koha::Patrons->find( { userid => $operation->{cardnumber} } );
 
     if ( $patron ) {
-        my $item = Koha::Items->find({ barcode => $operation->{barcode} });
+        my $item = Koha::Items->find( { barcode => $operation->{barcode}, homebranch => C4::Context->userenv->{'branch'} } );
         unless ($item) {
             return "Barcode not found.";
         }
@@ -4339,10 +4363,14 @@ sub ProcessOfflinePayment {
 sub TransferSlip {
     my ($branch, $itemnumber, $barcode, $to_branch) = @_;
 
+    my $item_filter = { barcode => $barcode };
+    if ( C4::Context->preference('IndependentBranches') ) {
+        $item_filter->{homebranch} = C4::Context->userenv->{'branch'};
+    }
     my $item =
       $itemnumber
       ? Koha::Items->find($itemnumber)
-      : Koha::Items->find( { barcode => $barcode } );
+      : Koha::Items->find( $item_filter );
 
     $item or return;
 
