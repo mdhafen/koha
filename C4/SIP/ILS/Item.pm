5e89c384685e6ea52f2069b3c8ef28b0f663e10c
Michael Hafen <michael.hafen@washk12.org>
2021-06-08 16:47:13 -0600

    Make item barcode unique just in homebranch
    Had to change a lot of sql and Koha::Item->find stuff to include homebranch.
    This is to enable migration of multiple sites from individual systems into
    a shared Koha instance.

--- C4/SIP/ILS/Item.pm
+++ C4/SIP/ILS/Item.pm
@@ -75,7 +75,11 @@ use Koha::Patrons;
 sub new {
     my ($class, $item_id) = @_;
     my $type = ref($class) || $class;
-    my $item = Koha::Items->find( { barcode => barcodedecode( $item_id ) } );
+    my $item_filter = { barcode => barcodedecode( $item_id ) };
+    if ( C4::Context->preference('IndependentBranches') ) {
+        $item_filter->{homebranch} = C4::Context->userenv->{'branch'};
+    }
+    my $item = Koha::Items->find( $item_filter );
     unless ( $item ) {
         siplog("LOG_DEBUG", "new ILS::Item('%s'): not found", $item_id);
         warn "new ILS::Item($item_id) : No item '$item_id'.";
