ab47fff0d53525c6872b17ee528cb3e1f8c077f1
Michael Hafen <michael.hafen@washk12.org>
2021-07-02 11:03:08 -0600

    Bug 28657: expand branches.branchip to allow for multiple ip ranges.
    change branches.branchip to a mediumtext, and remove '*' from existing branchip settings.
    Change C4/Auth.pm from a regexp to using in_iprange() to check client address against branchip.  (in_iprange() doesn't work with '*' in branchip.)
    Change in_iprange() to default to false (which is what the POD says it should do).  Also, have it trim '*' in case someone adds some back later.
    Change note in admin/branches.tt under branchip field to reflect changes to branchip handling.
    
    Test plan:
    
    1. Turn on AutoLocation system preference
    2. Change a branches branchip to multiple ip ranges seperated by a space,
       making sure one of the ranges includes your computers address.
    3. Log out and try logging in.  The location check will fail.
    4. Apply patch.  Restart plack if necessary.
    5. Try logging in again.  The location check will succeed.

--- C4/Auth.pm
+++ C4/Auth.pm
@@ -1233,9 +1233,7 @@ sub checkauth {
                         if ( C4::Context->preference('StaffLoginRestrictLibraryByIP') ) {
                             # we have to check they are coming from the right ip range
                             my $domain = $branches->{$branchcode}->{'branchip'} // q{};
-                            $domain =~ s|\.\*||g;
-                            $domain =~ s/\s+//g;
-                            if ( $domain && $ip !~ /^$domain/ ) {
+                            if ( $domain && ! in_iprange($domain) ) {
                                 $cookie = $cookie_mgr->replace_in_list( $cookie, $query->cookie(
                                     -name     => 'CGISESSID',
                                     -value    => '',
@@ -1268,7 +1266,8 @@ sub checkauth {
 
                                 #     now we work with the treatment of ip
                                 my $domain = $branches->{$br}->{'branchip'};
-                                if ( $domain && $ip =~ /^$domain/ ) {
+                                next unless ( $domain );
+                                if ( in_iprange($domain) ) {
                                     $branchcode = $branches->{$br}->{'branchcode'};
 
                                     # new op dev : add the branchname to the cookie
@@ -1740,7 +1739,8 @@ sub check_api_auth {
 
                     #     now we work with the treatment of ip
                     my $domain = $branches->{$br}->{'branchip'};
-                    if ( $domain && $ip =~ /^$domain/ ) {
+                    next unless ( $domain );
+                    if ( in_iprange($domain) ) {
                         $branchcode = $branches->{$br}->{'branchcode'};
 
                         # new op dev : add the branchname to the cookie
@@ -2371,9 +2371,11 @@ Returns 1 if the remote address is in the provided iprange, or 0 otherwise.
 
 sub in_iprange {
     my ($iprange) = @_;
-    my $result = 1;
+    my $result = 0;
+    # FIXME remove '*' for backwards compatibility in branchip settings
+    $iprange =~ s|\*||g;
     my @allowedipranges = $iprange ? split(' ', $iprange) : ();
-    if (scalar @allowedipranges > 0) {
+    if (@allowedipranges) {
         my @rangelist;
         eval { @rangelist = Net::CIDR::range2cidr(@allowedipranges); }; return 0 if $@;
         eval { $result = Net::CIDR::cidrlookup($ENV{'REMOTE_ADDR'}, @rangelist) } || Koha::Logger->get->warn('cidrlookup failed for ' . join(' ',@rangelist) );
