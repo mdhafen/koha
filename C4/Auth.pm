f7d1f5745773ba69df04157165dd13d8b3655b40
Michael Hafen <michael.hafen@washk12.org>
2021-09-10 10:04:32 -0600

    On Login/API in IndepBranch mode don't set the session branch by branchip

--- C4/Auth.pm
+++ C4/Auth.pm
@@ -1349,6 +1349,7 @@ sub checkauth {
                             }
                         }
 
+                        unless ( C4::Context->preference('IndependentBranches') && $branchcode ) {
                         if (
                             # If StaffLoginLibraryBasedOnIP is enabled we will try to find a branch
                             # matching your ip, regardless of the choice you have passed in
@@ -1378,6 +1379,7 @@ sub checkauth {
                                     last;
                                 }
                             }
+                        }
                         }
                     }
 
@@ -1859,16 +1861,18 @@ sub check_api_auth {
                     $branchname = $library ? $library->branchname : '';
                 }
                 my $branches = { map { $_->branchcode => $_->unblessed } Koha::Libraries->search->as_list };
-                foreach my $br ( keys %$branches ) {
+                unless ( $branchcode && C4::Context->preference('IndependentBranches') ) {
+                    foreach my $br ( keys %$branches ) {
 
-                    #     now we work with the treatment of ip
-                    my $domain = $branches->{$br}->{'branchip'};
-                    next unless ( $domain );
-                    if ( in_iprange($domain) ) {
-                        $branchcode = $branches->{$br}->{'branchcode'};
+                        #     now we work with the treatment of ip
+                        my $domain = $branches->{$br}->{'branchip'};
+                        next unless ( $domain );
+                        if ( in_iprange($domain) ) {
+                            $branchcode = $branches->{$br}->{'branchcode'};
 
-                        # new op dev : add the branchname to the cookie
-                        $branchname = $branches->{$br}->{'branchname'};
+                            # new op dev : add the branchname to the cookie
+                            $branchname    = $branches->{$br}->{'branchname'};
+                        }
                     }
                 }
                 $session->param( 'number',       $borrowernumber );
