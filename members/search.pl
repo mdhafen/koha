5aebbb78aeebee3e551bf9c8d398b896301bfb70
Michael Hafen <michael.hafen@washk12.org>
2023-06-30 11:15:20 -0600

    Bug 28726: local follow-up: preload sort1 and sort2 from the database
    Pre-load sort1 and sort2 from the database if the authorized values for them
    are empty.
    This is used by the Patron Card Creator
    
    Follow-up after Bug 34092 changed how the data table filters work here
    
    I change how the display of the sort fields is triggered.  Add them to
    the form fields directly instead of with their own input.
    In buildPatronSearchQuery() the early bail needs to return an array.  If
    it doesn't the code that adds the sort fields to the filters runs into an
    undefined variable instead of an array.

--- members/search.pl
+++ members/search.pl
@@ -22,6 +22,9 @@ use C4::Context;
 use C4::Auth qw( get_template_and_user );
 use C4::Output qw( output_html_with_http_headers );
 use Koha::Patron::Attribute::Types;
+use List::MoreUtils qw( uniq );
+use C4::Koha qw( GetAuthorisedValues );
+
 
 my $input = CGI->new;
 
@@ -39,6 +42,25 @@ my @columns = split ',', $input->param('columns');
 my $callback = $input->param('callback');
 my $selection_type = $input->param('selection_type') || 'select';
 my $filter = $input->param('filter');
+my @form_filters = split ',', $input->param('form_filters');
+unless (@form_filters) { @form_filters = ('branch','category') };
+
+my $sort_filter = { ( C4::Context->only_my_library ? (branchcode => C4::Context->userenv->{branch}) : () ) };
+my ( @sort1, @sort2, $pat_search_rs );
+if ( grep { $_ eq 'sort1' } @form_filters ) {
+    @sort1 = map { $_->{lib} } @{ GetAuthorisedValues("Bsort1") };
+    unless ( @sort1 ) {
+        $pat_search_rs = Koha::Patrons->search($sort_filter);
+        @sort1 = sort {$a cmp $b} uniq( $pat_search_rs->get_column('sort1') );
+    }
+}
+if ( grep { $_ eq 'sort2' } @form_filters ) {
+    @sort2 = map { $_->{lib} } @{ GetAuthorisedValues("Bsort2") };
+    unless ( @sort2 ) {
+        unless ($pat_search_rs) { $pat_search_rs = Koha::Patrons->search($sort_filter) };
+        @sort2 = sort {$a cmp $b} uniq( $pat_search_rs->get_column('sort2') );
+    }
+}
 
 $template->param(
     view           => ( $input->request_method() eq "GET" ) ? "show_form" : "show_results",
@@ -47,5 +69,8 @@ $template->param(
     filter         => $filter,
     selection_type => $selection_type,
     patron_search_unfiltered => ($input->param('unfiltered') ? "1" : "" ),
+    form_filters    => \@form_filters,
+    sort1           => \@sort1,
+    sort2           => \@sort2,
 );
 output_html_with_http_headers( $input, $cookie, $template->output );
